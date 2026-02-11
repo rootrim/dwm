6c5cccd3656589ac83d851443588f85be1af11c2
nixtrim <omuaz01@gmail.com>
2026-02-11 21:23:51 +0300

    removed dmenu

--- dwm.c
+++ dwm.c
@@ -1699,8 +1699,6 @@ void showhide(Client *c) {
 void spawn(const Arg *arg) {
   struct sigaction sa;
 
-  if (arg->v == dmenucmd)
-    dmenumon[0] = '0' + selmon->num;
   if (fork() == 0) {
     if (dpy)
       close(ConnectionNumber(dpy));
